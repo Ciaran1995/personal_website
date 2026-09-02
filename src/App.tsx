6b235d1b09e15b92c38a2e9c9ecd656ca165fe65
Ciaran1995 <ciaran.19952@gmail.com>
2026-09-02 17:23:02 +0100

    updating imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState, navigate } from 'react'
 import { Link, NavLink, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from 'public/vite.svg'
 import './App.css'
-import CertificatesPage from "./pages/CertificatesPage.jsx"
-import Header from "./components/Header.jsx"
+import CertificatesPage from "./pages/CertificatesPage"
+import Header from "./components/Header"
 
 const certificate = {
     image: "CertificateOfCompletion_JavaScript_Essential_Training.png",
